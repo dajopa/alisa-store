5a32157ff2c5f5fceb17665408f3466095c1d82f
dajopa <87281667+dajopa@users.noreply.github.com>
2026-05-05 06:39:47 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // 👈 ЭТО ГЛАВНОЕ
   images: {
-    unoptimized: true, // 👈 обязательно для static
     remotePatterns: [
       { protocol: "https", hostname: "nikifilini.com", pathname: "/**" },
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
